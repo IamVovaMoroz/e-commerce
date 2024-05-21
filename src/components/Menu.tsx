ce5b60ce10751e8c83d977e033605d2888e16e79
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2024-05-21 20:16:40 +0200

    menu mobile, SerchBar..

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,19 +1,35 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import { useState } from 'react'
 
 const Menu = () => {
+  const [open, setOpen] = useState(false)
 
-	const [open, setOpen] = useState(false)
-
-	return (
-		<div className=''>Menu
-<Image src='/menu.png' alt='menu button' width={28} height={28} className='cursor-pointer'/>
-
-
-		</div>
-	)
+  return (
+    <div className=''>
+      <Image 
+        src='/menu.png' 
+        alt='menu button' 
+        width={28} 
+        height={28} 
+        className='cursor-pointer' 
+        onClick={() => setOpen(prev => !prev)} 
+      />
+      {open && (
+        <div className='absolute bg-black text-white left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 text-xl z-10'>
+          <Link href='/'>Homepage</Link>
+          <Link href='/shop'>Shop</Link>
+          <Link href='/deals'>Deals</Link>
+          <Link href='/about'>About</Link>
+          <Link href='/contact'>Contact</Link>
+          <Link href='/logout'>Logout</Link>
+          <Link href='/cart'>Cart(1)</Link>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Menu
